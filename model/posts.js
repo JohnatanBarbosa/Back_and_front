8344a76f5685f7d7ca2b9ac1f615b961ddd6fb3d
JohnatanBarbosa <johnatanbb99@gmail.com>
2021-12-01 13:55:57 -0300

    primeiro commit

---
+++ model/posts.js
@@ -0,0 +1,19 @@
+module.exports = {
+  posts: [
+    { id: 'aiqwe', title: 'Teste do Mural', description: 'Descrição teste' }
+  ],
+
+  getAll() {
+    return this.posts
+  },
+
+  newPost(title, description) {
+    this.posts.push({ id: generateID(), title, description })
+  },
+
+  deletePost(id) {}
+}
+
+function generateID() {
+  return Math.random().toString(36).substr(2, 9)
+}
